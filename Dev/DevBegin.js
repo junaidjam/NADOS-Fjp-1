e444014766555b7daf16baa01388b3c246d255db
junaidjam <junaidakhtarjam@gmail.com>
2022-02-26 12:02:40 +0530

    initial commit

--- Dev/DevBegin.js
+++ Dev/DevBegin.js
@@ -1,5 +1,12 @@
+//output
 console.log("hello world");
+
+//variables
+// you can initialize i by any type of data
+// java scrript is dynamically typed language
 let i = 10;
 console.log(i);
 i = "junaid akhtar";
 console.log(i);
+
+
---
+++ Dev/input.js
@@ -0,0 +1,6 @@
+let args = process.argv;
+//console.log(args);
+
+
+let i = args[2];
+console.log(i);
