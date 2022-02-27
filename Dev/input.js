bfa0e90d97981b0d754d2e264c16e4a3c77599e2
junaidjam <junaidakhtarjam@gmail.com>
2022-02-27 12:54:13 +0530

    initial commit

--- Dev/input.js
+++ Dev/input.js
@@ -2,5 +2,17 @@ let args = process.argv;
 //console.log(args);
 
 
-let i = args[2];
-console.log(i);
+// let i = args[2];
+// console.log(i);
+
+// everythin is coming as a string
+// let i = args[2];
+// console.log[i];
+// console.log(typeof i);
+// i = i+30;
+// console.log(i);
+
+
+let j = parseInt("200" , 10);
+j = j + 30;
+console.log(j);
