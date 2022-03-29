e81740115ae973aa7b37697ba7d5ec8135d7a85f
junaidjam <junaidakhtarjam@gmail.com>
2022-03-29 22:35:46 +0530

    initial commit

---
+++ Datastructure and Algorithm/Basics of Programming/Recursion And Backtracking For Beginners/Power Linear.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        // write your code here
+        Scanner scn = new Scanner(System.in);
+        int x = scn.nextInt();
+        int n = scn.nextInt();
+        System.out.print(power(x,n));
+    }
+
+    public static int power(int x, int n){
+        if(n == 0){
+            return 1;
+        }
+        int mul = power(x , n-1);
+        mul = x * mul;
+        return mul;
+    }
+
+}
